d99f5bc58c5f123aca387aa936ba1dde223853de
Kesava <Kesava@gmail.com>
2023-10-23 10:56:32 +0530

    cucumber project added

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"e39a3724-2cff-421a-bc2b-99fece0762ca","feature":"Testing rediff myPage","scenario":"User has to test if login on rediff is successful or not","start":1697798061324,"group":1,"content":"","tags":"@register,","end":1697798081349,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
